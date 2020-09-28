71265d3cd7130e5f4bd4f2ee23aae1cfd53cb581
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-28 10:27:47 +0200

    values

--- console.sql
+++ console.sql
@@ -1,2 +1,3 @@
-SELECT first_name, salary, round(salary), floor(salary), ceiling(salary)
-from employee
+insert into  employee values (1002, 'Placide', 'Voodoo', 'Male', null, 1)
+insert into  employee values (1003, 'Muteba', 'Gizenga', 'Male', null, 1)
+insert into  employee values (1004, 'Mbole', 'Ebunike', 'Male', null, 1)
