fa235a63522a8c3abf5941ad087e6b0c664acd7e
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-28 10:50:49 +0200

    substring

--- console.sql
+++ console.sql
@@ -1,3 +1,1 @@
-insert into  employee values (1002, 'Placide', 'Voodoo', 'Male', null, 1)
-insert into  employee values (1003, 'Muteba', 'Gizenga', 'Male', null, 1)
-insert into  employee values (1004, 'Mbole', 'Ebunike', 'Male', null, 1)
+select substring(first_name, 1, 1) from employee
